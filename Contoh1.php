598fdfe12e91294009684430452832404ac714ae
hendri1907 <hendridarmawan0704@gmail.com>
2024-03-22 15:05:42 +0700

    pertama

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>INI ADALAH CONTOH DEFAULT CONTROLLER </h1>";
+	}
+}
---
+++ Tes.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Tes extends CI_Controller {
+
+
+	public function index () {
+		echo "Helloda";
+	}
+}
+
+?>
